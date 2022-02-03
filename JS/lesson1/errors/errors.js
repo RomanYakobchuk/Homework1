97aae128ea976ffd75b0b986da827d85c9663035
RomanYakobchuk <romanyakobchuk28@gmail.com>
2022-02-03 16:31:05 +0200

    lesson2

---
+++ JS/lesson1/errors/errors.js
@@ -0,0 +1,17 @@
+
+    // Ваше завдання, зробити так, щоб файл відкривався без помилок .
+
+    let a = 100;
+    let b = 500;
+    let c = "hello";
+    let d = "okten";
+    let x = 'constant value';
+    {
+        console.log(x);
+        x = 'new value';
+    }
+    c = 'new value';
+
+    let result = 100 + b;
+
+    console.log(result);
