e18cc59dbbe7112081ef488de1fb038b2b44e15d
RomanYakobchuk <romanyakobchuk28@gmail.com>
2022-02-04 18:14:44 +0200

    lesson3

---
+++ JS/lesson3/classwork3/main.js
@@ -0,0 +1,109 @@
+//зробити масив з 10 чисел [2,17,13,6,22,31,45,66,100,-18]та:
+let arr = [2,17,13,6,22,31,45,66,100,-18];
+// 1. перебрати його циклом while
+let i = 0;
+while(i < arr.length) {
+    console.log('цикл while -',arr[i]);
+    i++;
+}
+
+// 2. перебрати його циклом for
+for(let n = 0; n < arr.length; n++){
+    console.log('цикл for -', arr[n]);
+}
+
+// 3. перебрати циклом while та вивести числа тільки з непарним індексом
+let u = 0;
+while (u < arr.length){
+    if([u] % 2 === 1){
+        console.log('цикл while з непарним індексом-', ` ${u}`, arr[u]);
+    }
+    u++;
+}
+
+// 4. перебрати циклом for та вивести числа тільки з непарним індексом
+for(let y = 0; y < arr.length; y++){
+    if([y] % 2 === 1){
+        console.log('цикл for з непарним індексом-', ` ${y}`, arr[y]);
+    }
+}
+
+// 5. перебрати циклом while та вивести числа тільки парні значення
+let g = 0;
+while (g < arr.length){
+    if([g] % 2 === 0){
+        console.log('цикл while з парним індексом-', ` ${g}`, arr[g]);
+    }
+    g++;
+}
+
+// 6. перебрати циклом for та вивести числа тільки парні значення
+for(let q = 0; q < arr.length; q++){
+    if([q] % 2 === 0){
+        console.log('цикл for з парним індексом-', ` ${q}`, arr[q]);
+    }
+}
+
+// 7. замінити кожне число кратне 3 на слово "okten"
+for(let w = 0; w < arr.length; w++){
+    if(arr[w] % 3 === 0){
+        arr[w] = 'okten';
+        console.log('число кратне 3 -', ` ${w}`, arr[w]);
+    }
+}
+
+// 8. вивести масив у зворотному порядку.
+for(let r = arr.length; r >= 0; r--){
+    console.log('зворотній порядок:', arr[r]);
+}
+
+
+// 9. всі попередні завдання (окрім 8), але у зворотному циклі (с заду на перед)
+
+let i2 = arr.length;
+while(i2 >= 0) {
+    console.log('цикл while -',arr[i2]);
+    i2--;
+}
+
+
+for(let n = arr.length; n >= 0; n--){
+    console.log('цикл for -', arr[n]);
+}
+
+let u2 = arr.length;
+while (u2 >= 0){
+    if([u2] % 2 === 1){
+        console.log('цикл while з непарним індексом-', ` ${u2}`, arr[u2]);
+    }
+    u2--;
+}
+
+for(let y = arr.length; y >= 0; y--){
+    if([y] % 2 === 1){
+        console.log('цикл for з непарним індексом-', ` ${y}`, arr[y]);
+    }
+}
+
+let g2 = arr.length;
+while (g2 >= 0){
+    if([g2] % 2 === 0){
+        console.log('цикл while з парним індексом-', ` ${g2}`, arr[g2]);
+    }
+    g2--;
+}
+
+for(let q2 = arr.length; q2 >= 0; q2--){
+    if([q2] % 2 === 0){
+        console.log('цикл for з парним індексом-', ` ${q2}`, arr[q2]);
+    }
+}
+
+
+for(let w = arr.length; w >= 0; w--){
+    if(arr[w] % 3 === 0){
+        arr[w] = 'okten';
+        console.log('число кратне 3 -', ` ${w}`, arr[w]);
+    }
+}
+
---
+++ JS/lesson3/homework3/main.js
@@ -0,0 +1,155 @@
+//--створити масив з:
+//    - з 5 числових значень
+//    - з 5 стрічкових значень
+//    - з 5 значень стрічкового, числового та булевого типу
+//    - та вивести його в консоль
+let arrInt = [5, 6, 56, 345, -54];
+let arrStr = ['Kiev', 'Lviv', 'Khmelnytskyi', 'Chernihiv', 'Odessa'];
+let arrAll = [4, 'Kiev', true, 'Okten', 2022];
+console.log(arrInt);
+console.log(arrStr);
+console.log(arrAll);
+
+
+
+
+//-- Створити пустий масив. Наповнити його будь-якими значеннями звертаючись до конкретного індексу. Вивести в консоль
+let array = [];
+array[0] = '0_o';
+array[1] = 2022;
+array[2] = true;
+array[3] = '<3';
+array[4] = '/(*_*)/';
+array[5] = 988;
+console.log(array);
+
+
+
+//- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+//- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+//- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+//- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+for(let i = 0; i < 10; i++){
+    document.write(`<div>text</div>`);
+}
+for(let i = 0; i < 10; i++){
+    document.write(`<div>txt lorem ipsun in - ${i}</div>`);
+}
+let i = 0;
+while (i < 20){
+    document.write(`<h1>snflvnsovnkjvn erevrtv bryberybe</h1>`)
+    i++;
+}
+let y = 0;
+while (y < 20){
+    document.write(`<h1>${y} - snflvnsovnkjvn erevrtv bryberybe</h1>`)
+    y++;
+}
+
+
+
+
+
+//- Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+//- Створити масив з 10 строкових елементів. Вивести в консоль всі його елементи в циклі.
+//- Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+//- Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
+//- Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
+//- Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи
+let numArray = [23, 544, 0, -45, 6.3, 1000, 55, 6843, 6, 233];
+for(let i = 0; i < numArray.length; i++){
+    console.log(numArray[i]);
+}
+//=============
+let strArray = ['one', 'two', 'tree', 'qwerty', 'Okten', 'ASUS', 'Apple', 'Google', 'Name', 'Kiev'];
+for(let i = 0; i < strArray.length; i++){
+    console.log(strArray[i]);
+}
+//=============
+let allArray = ['one', 2, true, 'qwerty', 'Okten', 34545345, 'Apple', 'Google', false, 'Kiev'];
+for(let i = 0; i < allArray.length; i++){
+    console.log(allArray[i]);
+}
+//=============
+let allTwoArray = ['one', 2, true, 'qwerty', 'Okten', 34545345, 'Apple', 'Google', false, 'Kiev'];
+for(let i = 0; i < allTwoArray.length; i++){
+    if(typeof allTwoArray[i] === 'boolean'){
+        console.log('boolean - ', allTwoArray[i]);
+    }
+}
+//=============
+let allNumArray = ['one', 2, true, 'qwerty', 'Okten', 34545345, 'Apple', 'Google', false, 'Kiev'];
+for(let i = 0; i < allNumArray.length; i++){
+    if(typeof allNumArray[i] === 'number'){
+        console.log('number - ', allNumArray[i]);
+    }
+}
+//=============
+let allStrArray = ['one', 2, true, 'qwerty', 'Okten', 34545345, 'Apple', 'Google', false, 'Kiev'];
+for(let i = 0; i < allStrArray.length; i++){
+    if(typeof allStrArray[i] === 'string'){
+        console.log('string - ', allStrArray[i]);
+    }
+}
+//=============
+
+
+
+
+//- Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів. Вивести в консоль всі його елементи в циклі.
+let ArrayArr = [];
+ArrayArr[0] = true;
+ArrayArr[1] = 4554;
+ArrayArr[2] = 19;
+ArrayArr[3] = 'Yeees!!!';
+ArrayArr[4] = false;
+ArrayArr[5] = 'QWERTY';
+ArrayArr[6] = 'OKTEN';
+ArrayArr[7] = 2022;
+ArrayArr[8] = 1991;
+ArrayArr[9] = 'KIEV';
+for(let i = 0; i < ArrayArr.length; i++){
+    console.log(ArrayArr[i]);
+}
+//====================================
+//- Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+for(let i = 0; i < 10; i++){
+    console.log('крок циклу:  '+ i +' ; ');
+    document.write('крок циклу: '+ i +'; ');
+}
+//====================================
+//- Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+for(let i = 0; i < 100; i++){
+    console.log('крок циклу:  '+ i +' ; ');
+    document.write('крок циклу: '+ i +'; ');
+}
+//====================================
+
+
+//- Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
+for(let i = 0; i < 100; i+=2){
+    console.log('крок циклу:  '+ i +' ; ');
+    document.write('крок циклу: '+ i +'; ');
+}
+//====================================
+
+//- Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+for(let i = 0; i < 100; i++){
+   if(i % 2 === 0){
+       console.log('крок циклу:  '+ i +' ; ');
+       document.write('крок циклу: '+ i +'; ');
+   }
+}
+//====================================
+
+
+
+//- Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+for(let i = 0; i < 100; i++){
+    if(i % 2 === 1){
+        console.log('крок циклу:  '+ i +' ; ');
+        document.write('крок циклу: '+ i +'; ');
+    }
+}
+//====================================
+
